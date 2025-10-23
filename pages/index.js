9b400a2f57c7d3e6c8ff09001a6a156a57a828d6
mi <43147648+mileine@users.noreply.github.com>
2025-10-23 01:47:43 +0000

    adiciona `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
+import { convertRouteTreeToFlightRouterState } from "next/dist/client/components/segment-cache-impl/cache";
+
 function Home() {
-    return <h1>“Anjin... vc é muito lindo!😍😍😍”</h1>
+  return <h1>“Anjin... vc é muito lindo!😍😍😍”</h1>;
 }
 
 export default Home;
