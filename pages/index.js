81443d7ee47489d4776c1bf9835c209caaa432db
mi <43147648+mileine@users.noreply.github.com>
2025-10-23 23:47:50 +0000

    remove unused import

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-import { convertRouteTreeToFlightRouterState } from "next/dist/client/components/segment-cache-impl/cache";
-
 function Home() {
   return <h1>“Anjin... vc é muito lindo!😍😍😍”</h1>;
 }
